ed99b7e5bc271c099d4c6662639dc5c492ee4742
Amit <amitbhagat.h@gmail.com>
2019-09-08 22:40:31 +0530

    https://github.com/AmitHBhagat/Hello-World-2.git committing files

---
+++ home.js
@@ -0,0 +1,7 @@
+(function(email){
+  const fname = 'Amit';
+  const lname = 'Bhagat';
+  const fullName = () => fname + lname;
+  console.log(`My name is ${fullName()}`);
+  console.log(`My Email id is ${email}`);
+})('amitbhagat@gmail.com');
